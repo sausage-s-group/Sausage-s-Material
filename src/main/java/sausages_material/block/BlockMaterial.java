f210d8d1693e78a398a677808138f351e5d9e674
InitAuther97 <1602375324@qq.com>
2019-08-21 20:08:00 +0800

    Added Meta-State conversation

--- src/main/java/sausages_material/block/BlockMaterial.java
+++ src/main/java/sausages_material/block/BlockMaterial.java
@@ -4,22 +4,29 @@ import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import sausages_material.api.IMaterial;
 import sausages_material.api.MetalShapes;
 
 @MethodsReturnNonnullByDefault
-public class BlockMaterial extends Block
+public class BlockMaterial<T extends Enum<T>& IMaterial& IStringSerializable> extends Block
 {
     public static final PropertyInteger TYPE = PropertyInteger.create("material",0,19);
+    private final Class<T> clz;
+    private final PropertyEnum<T> MATERIAL;
 
-    public BlockMaterial(MetalShapes material) {
+    public BlockMaterial(Class<T> clz,PropertyEnum<T> material) {
         super(Material.IRON,MapColor.IRON);
+        this.clz = clz;
+        this.MATERIAL = material;
     }
 
     @Override
@@ -28,6 +35,16 @@ public class BlockMaterial extends Block
     }
     @Override
     public BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this,TYPE);
+        return new BlockStateContainer(this,MATERIAL);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state){
+        return state.getValue(MATERIAL).ordinal();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta){
+        return  getDefaultState().withProperty(MATERIAL,clz.getEnumConstants()[meta]);
     }
 }
